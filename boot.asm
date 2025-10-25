3a8225afdbd09a19d7da2529b56aac388b9d9773
Alexia <alexia.reyara@gmail.com>
2025-10-25 13:00:12 +0200

    Create boot.asm

---
+++ boot.asm
@@ -0,0 +1,32 @@
+bits 16
+org 0x7c00
+
+start:
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+    mov bx, 0x1000
+    mov es, ax
+
+    mov ah, 0x02
+    mov al, 0x20
+    mov ch, 0x00
+    mov cl, 0x02
+    mov dh, 0x00
+
+    int 0x13
+    jc disk_error
+
+    jmp 0x0000:0x1000
+
+disk_error:
+    hlt
+    jmp disk_error
+
+times 510-($-$$) db 0
+dw 0xAA55
